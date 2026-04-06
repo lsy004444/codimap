98cede12a4b02001f9376498bc454729c4796836
seoyoung <a-010557@daum.net>
2026-04-06 07:06:42 +0000

    상단 검색바 설치

--- public/js/map.js
+++ public/js/map.js
@@ -3,8 +3,8 @@ window.onload = function() {
     kakao.maps.load(function() {
         var container = document.getElementById('map');
         var options = {
-            center: new kakao.maps.LatLng(37.5665, 126.9780),
-            level: 3
+            center: new kakao.maps.LatLng(36.2683, 127.6358),
+            level: 13
         };
         var map = new kakao.maps.Map(container, options);
     });
