7c9c2ada19bd63e288a6ab0fadde97433a11cd31
seoyoung <a-010557@daum.net>
2026-04-12 15:34:49 +0000

    검색창에 주소 검색 시 지도 확대 & 마커 표시

--- public/js/map.js
+++ public/js/map.js
@@ -6,6 +6,45 @@ window.onload = function() {
             level: 13
         };
         var map = new kakao.maps.Map(container, options);
+
+        var geocoder = new kakao.maps.services.Geocoder();
+        function searchLocation(keyword) {
+
+            geocoder.addressSearch(keyword, function(result, status){
+                if(status === kakao.maps.services.Status.OK) {
+                    var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                    var fullAddress = result[0].address_name;
+                    var regionName = extractDongName(fullAddress);
+                    console.log("검색된 지역이름: ",regionName);
+
+                    var marker = new kakao.maps.Marker({
+                        map: map,
+                        position: coords
+                    });
+                    map.setCenter(coords);
+                    map.setLevel(5);
+
+                    var infowindow = new kakao.maps.InfoWindow({
+                        content: `<div style="width:150px;text-align:center;padding:6px 0;">${regionName}</div>`
+                    });
+                    infowindow.open(map, marker);
+                }
+            })
+        }
+        function extractDongName(address) {
+            const match = address.match(/([가-힣]+(동|면|읍)) (?=\s|$)/);
+            return match ? match[1] : address;
+        }
+
+        const searchInput = document.querySelector('.search-box input');
+        searchInput.addEventListener('keypress', function(e) {
+            if(e.key === 'Enter') {
+                searchLocation(this.value);
+            }
+        });
+        initSeasonButtons();
+    
     });
 };
 
@@ -56,5 +95,6 @@ function initSeasonButtons() {
         container.appendChild(btn);
     });
 }
+
 
 window.addEventListener('DOMContentLoaded', initSeasonButtons);
