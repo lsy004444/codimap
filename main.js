33cd2b57e8752ccb9cf305ec362eb929636e5e5f
ayj089 <ayj089@naver.com>
2026-05-17 01:55:10 +0900

    충돌 해결 완료

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express'),
       path = require('path'),
       app = express();
@@ -12,6 +11,7 @@ app.get('/', (req, res) => {
         </div>`
     );
 });
+
 
 app.get('/map', (req , res) => {
     res.sendFile(path.join(__dirname, 'views', 'map.html'));
@@ -24,26 +24,4 @@ app.get('/feed', (req, res) => {
 })
 
 
-
-=======
-const express = require('express'),
-      path = require('path'),
-      app = express();
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.send(
-        `<div style="text-align:center; margin-top:50px;">
-           <h1> 80번 포트 실행중 </h1>
-        </div>`
-    );
-});
-
-app.get('/map', (req , res) => {
-    res.sendFile(path.join(__dirname, 'views', 'map.html'));
-})
-
-
->>>>>>> 870ae20 (프론트엔드 구현 완료)
 app.listen(80, () => console.log('✅ 서버 가동 중: http://localhost'));
