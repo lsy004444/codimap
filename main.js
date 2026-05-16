883de52a5f928bf40ce74bcfb5e2b53ca8ed009a
ayj089 <ayj089@naver.com>
2026-05-17 00:32:06 +0900

    프론트엔드 구현 완료

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express'),
       path = require('path'),
       app = express();
@@ -24,4 +25,25 @@ app.get('/feed', (req, res) => {
 
 
 
+=======
+const express = require('express'),
+      path = require('path'),
+      app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send(
+        `<div style="text-align:center; margin-top:50px;">
+           <h1> 80번 포트 실행중 </h1>
+        </div>`
+    );
+});
+
+app.get('/map', (req , res) => {
+    res.sendFile(path.join(__dirname, 'views', 'map.html'));
+})
+
+
+>>>>>>> 870ae20 (프론트엔드 구현 완료)
 app.listen(80, () => console.log('✅ 서버 가동 중: http://localhost'));
