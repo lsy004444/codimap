918d35700e9e556dc078e6102c9797dbfda2305f
lsy004444 <a-010557@daum.net>
2026-06-09 22:31:16 +0900

    로그인 시 로그인, 회원가입 버튼 없애기. 회원가입 버튼 추가

--- public/js/map.js
+++ public/js/map.js
@@ -389,11 +389,22 @@ function handleMenuClick(e) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     // fade-in
     document.body.style.opacity = '0';
     document.body.style.transition = 'opacity 0.4s ease';
     setTimeout(() => document.body.style.opacity = '1', 50);
+
+    //로그인 상태 확인 
+    const authRes = await fetch('/api/auth/mypage');
+    const authData = await authRes.json();
+
+    if (authData.success) {
+        document.getElementById('loginBtn').style.display = 'none';
+        document.getElementById('signupBtn').style.display = 'none';
+        document.getElementById('divider1').style.display = 'none';
+        document.getElementById('divider2').style.display = 'none';
+    }
 
     // 로그아웃
     const logoutBtn = document.getElementById("logoutBtn");
